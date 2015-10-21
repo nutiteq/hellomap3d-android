a44ac12a82adfb0b1d234bc0bf0b236c4c6a8934
Jaak <jaak@nutiteq.com>
2015-10-21 23:36:29 +0100

    SDK 3.2.1 release

--- com.nutiteq.advancedmap3/src/com/nutiteq/advancedmap3/Const.java
+++
@@ -1,7 +0,0 @@
-package com.nutiteq.advancedmap3;
-
-public class Const {
-	public static final String LOG_TAG = "nutiteq-android-sample";
-	public static final String MAPBOX_RASTER_URL = "http://api.tiles.mapbox.com/v3/nutiteq.map-j6a1wkx0/{zoom}/{x}/{y}.png";
-	public static final String HILLSHADE_RASTER_URL = "http://tiles.wmflabs.org/hillshading/{zoom}/{x}/{y}.png";
-}
