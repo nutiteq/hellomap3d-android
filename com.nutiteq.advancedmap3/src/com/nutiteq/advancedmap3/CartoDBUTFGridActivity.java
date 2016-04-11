bd20e22d36d392efa638667544e2750b5dedfd0a
Jaak <jaak@nutiteq.com>
2016-04-11 13:49:28 +0300

    create map config request dynamically

--- com.nutiteq.advancedmap3/src/com/nutiteq/advancedmap3/CartoDBUTFGridActivity.java
+++ com.nutiteq.advancedmap3/src/com/nutiteq/advancedmap3/CartoDBUTFGridActivity.java
@@ -1,7 +1,9 @@
 package com.nutiteq.advancedmap3;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.nutiteq.advancedmap3.listener.MyMapEventListener;
@@ -12,6 +14,7 @@ import com.nutiteq.datasources.LocalVectorDataSource;
 import com.nutiteq.layers.UTFGridRasterTileLayer;
 import com.nutiteq.layers.VectorLayer;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -33,8 +37,122 @@ public class CartoDBUTFGridActivity extends VectorMapSampleBaseActivity {
         // MapSampleBaseActivity creates and configures mapView  
         super.onCreate(savedInstanceState);
 
-        GetLayerGroupID getLayerGroupID = new GetLayerGroupID();
-        getLayerGroupID.execute();
+        // define server config
+
+        try {
+
+            // you need to change these according to your DB
+
+            String cartoDbAccount = "nutiteq";
+            String sql = "select * from stations_1";
+            String statTag = "3c6f224a-c6ad-11e5-b17e-0e98b61680bf";
+            String[] columns = new String[]{"name","field_9","slot"};
+            String cartoCss =
+                    "/** category visualization */\n" +
+                    "\n" +
+                    "#stations_1 {\n" +
+                    "   marker-fill-opacity: 0.9;\n" +
+                    "   marker-line-color: #FFF;\n" +
+                    "   marker-line-width: 2;\n" +
+                    "   marker-line-opacity: 1;\n" +
+                    "   marker-placement: point;\n" +
+                    "   marker-type: ellipse;\n" +
+                    "   marker-width: 10;\n" +
+                    "   marker-allow-overlap: true;\n" +
+                    "}\n" +
+                    "\n" +
+                    "#stations_1[field_7=\"In Service\"] {\n" +
+                    "   marker-fill: #0F3B82;\n" +
+                    "}\n" +
+                    "#stations_1[field_7=\"Not In Service\"] {\n" +
+                    "   marker-fill: #aaaaaa;\n" +
+                    "}\n" +
+                    "\n" +
+                    "#stations_1 [ field_9 = 200] {\n" +
+                    "   marker-width: 80.0;\n" +
+                    "}\n" +
+                    "\n" +
+                    "#stations_1 [ field_9 <= 49] {\n" +
+                    "   marker-width: 25.0;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 38] {\n" +
+                    "   marker-width: 22.8;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 34] {\n" +
+                    "   marker-width: 20.6;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 29] {\n" +
+                    "   marker-width: 18.3;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 25] {\n" +
+                    "   marker-width: 16.1;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 20.5] {\n" +
+                    "   marker-width: 13.9;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 16] {\n" +
+                    "   marker-width: 11.7;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 12] {\n" +
+                    "   marker-width: 9.4;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 8] {\n" +
+                    "   marker-width: 7.2;\n" +
+                    "}\n" +
+                    "#stations_1 [ field_9 <= 4] {\n" +
+                    "   marker-width: 5.0;\n" +
+                    "}";
+
+
+            // you probably do not need to change much of below
+            JSONObject configJson = new JSONObject();
+
+            configJson.put("version", "1.0.1");
+            configJson.put("stat_tag", statTag);
+
+            JSONArray layersArrayJson = new JSONArray();
+            JSONObject layersJson = new JSONObject();
+            layersJson.put("type", "cartodb");
+
+            JSONObject optionsJson = new JSONObject();
+            optionsJson.put("sql", sql);
+            optionsJson.put("cartocss", cartoCss);
+            optionsJson.put("cartocss_version", "2.1.1");
+            JSONArray interactivityJson = new JSONArray();
+            interactivityJson.put("cartodb_id");
+            optionsJson.put("interactivity", interactivityJson);
+            JSONObject attributesJson = new JSONObject();
+            attributesJson.put("id", "cartodb_id");
+            JSONArray columnsJson = new JSONArray();
+            for(String col: columns){
+                columnsJson.put(col);
+            }
+
+            attributesJson.put("columns", columnsJson);
+            optionsJson.put("attributes",attributesJson);
+            layersJson.put("options",optionsJson);
+            layersArrayJson.put(layersJson);
+            configJson.put("layers", layersArrayJson);
+
+            String config = configJson.toString();
+
+            Log.i(Const.LOG_TAG, config);
+
+            Uri builtUri = Uri.parse("https://"+cartoDbAccount+".cartodb.com/api/v1/map")
+                    .buildUpon()
+                    .appendQueryParameter("config", config)
+                    .build();
+            URL url = new URL(builtUri.toString());
+
+            GetLayerGroupID getLayerGroupID = new GetLayerGroupID(url, cartoDbAccount);
+            getLayerGroupID.execute();
+
+        } catch (MalformedURLException e) {
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
@@ -46,9 +164,15 @@ public class CartoDBUTFGridActivity extends VectorMapSampleBaseActivity {
 
     private class GetLayerGroupID extends AsyncTask<Void, Void, Boolean> {
 
+        private final String cartoDbAccount;
         private URL url;
         private HttpURLConnection conn;
         private BufferedReader reader;
+
+        public GetLayerGroupID(URL url, String cartoDbAccount) {
+            this.cartoDbAccount = cartoDbAccount;
+            this.url = url;
+        }
 
 
         /**
@@ -58,12 +182,11 @@ public class CartoDBUTFGridActivity extends VectorMapSampleBaseActivity {
          * @return
          */
         protected Boolean doInBackground(Void... q) {
-            String link = "https://mforrest.cartodb.com/api/v1/map?config=%7B%22version%22%3A%221.0.1%22%2C%22stat_tag%22%3A%223c6f224a-c6ad-11e5-b17e-0e98b61680bf%22%2C%22layers%22%3A%5B%7B%22type%22%3A%22cartodb%22%2C%22options%22%3A%7B%22sql%22%3A%22select%20*%20from%20stations_1%22%2C%22cartocss%22%3A%22%5Cn%5Cn%2F**%20category%20visualization%20*%2F%5Cn%5Cn%23stations_1%20%7B%5Cn%20%20%20marker-fill-opacity%3A%200.9%3B%5Cn%20%20%20marker-line-color%3A%20%23FFF%3B%5Cn%20%20%20marker-line-width%3A%202%3B%5Cn%20%20%20marker-line-opacity%3A%201%3B%5Cn%20%20%20marker-placement%3A%20point%3B%5Cn%20%20%20marker-type%3A%20ellipse%3B%5Cn%20%20%20marker-width%3A%2010%3B%5Cn%20%20%20marker-allow-overlap%3A%20true%3B%5Cn%7D%5Cn%5Cn%23stations_1%5Bfield_7%3D%5C%22In%20Service%5C%22%5D%20%7B%5Cn%20%20%20marker-fill%3A%20%230F3B82%3B%5Cn%7D%5Cn%23stations_1%5Bfield_7%3D%5C%22Not%20In%20Service%5C%22%5D%20%7B%5Cn%20%20%20marker-fill%3A%20%23aaaaaa%3B%5Cn%7D%5Cn%5Cn%23stations_1%20%5B%20field_9%20%3D%20200%5D%20%7B%5Cn%20%20%20marker-width%3A%2080.0%3B%5Cn%7D%5Cn%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2049%5D%20%7B%5Cn%20%20%20marker-width%3A%2025.0%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2038%5D%20%7B%5Cn%20%20%20marker-width%3A%2022.8%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2034%5D%20%7B%5Cn%20%20%20marker-width%3A%2020.6%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2029%5D%20%7B%5Cn%20%20%20marker-width%3A%2018.3%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2025%5D%20%7B%5Cn%20%20%20marker-width%3A%2016.1%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2020.5%5D%20%7B%5Cn%20%20%20marker-width%3A%2013.9%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2016%5D%20%7B%5Cn%20%20%20marker-width%3A%2011.7%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%2012%5D%20%7B%5Cn%20%20%20marker-width%3A%209.4%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%208%5D%20%7B%5Cn%20%20%20marker-width%3A%207.2%3B%5Cn%7D%5Cn%23stations_1%20%5B%20field_9%20%3C%3D%204%5D%20%7B%5Cn%20%20%20marker-width%3A%205.0%3B%5Cn%7D%22%2C%22cartocss_version%22%3A%222.1.1%22%2C%22interactivity%22%3A%5B%22cartodb_id%22%5D%2C%22attributes%22%3A%7B%22id%22%3A%22cartodb_id%22%2C%22columns%22%3A%5B%22name%22%2C%22field_9%22%2C%22slot%22%5D%7D%7D%7D%5D%7D";
 
             boolean isOK = false;
 
             try {
-                url = new URL(link);
+
 
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setConnectTimeout(30000);
@@ -122,10 +245,10 @@ public class CartoDBUTFGridActivity extends VectorMapSampleBaseActivity {
         protected void onPostExecute(Boolean isOK) {
             if (isOK && !layerGroupID.equals("")) {
                 HTTPTileDataSource rasterDataSource = new HTTPTileDataSource(0, 19,
-                        "https://cartocdn-ashbu.global.ssl.fastly.net/mforrest/api/v1/map/"+layerGroupID+"/0/{zoom}/{x}/{y}.png");
+                        "https://cartocdn-ashbu.global.ssl.fastly.net/"+cartoDbAccount+"/api/v1/map/"+layerGroupID+"/0/{zoom}/{x}/{y}.png");
 
                 HTTPTileDataSource utfGridDataSource = new HTTPTileDataSource(0, 19,
-                        "https://cartocdn-ashbu.global.ssl.fastly.net/mforrest/api/v1/map/"+layerGroupID+"/0/{zoom}/{x}/{y}.grid.json");
+                        "https://cartocdn-ashbu.global.ssl.fastly.net/"+cartoDbAccount+"/api/v1/map/"+layerGroupID+"/0/{zoom}/{x}/{y}.grid.json");
 
                 UTFGridRasterTileLayer utfGridRasterTileLayer = new UTFGridRasterTileLayer(rasterDataSource, utfGridDataSource);
 
