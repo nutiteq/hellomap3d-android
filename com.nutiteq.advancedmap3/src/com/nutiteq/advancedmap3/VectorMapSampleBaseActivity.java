666bdeebc83b87ae819eae502a3b426627605c1c
Aare Undo <aare@carto.com>
2016-10-05 13:26:44 +0300

    style menu now contains nutiteq_dark and nutiteq_gray

--- com.nutiteq.advancedmap3/src/com/nutiteq/advancedmap3/VectorMapSampleBaseActivity.java
+++ com.nutiteq.advancedmap3/src/com/nutiteq/advancedmap3/VectorMapSampleBaseActivity.java
@@ -22,91 +22,102 @@ import com.nutiteq.wrappedcommons.UnsignedCharVector;
  */
 public class VectorMapSampleBaseActivity extends MapSampleBaseActivity {
 
-	public static final String MAIN_STYLE = "nutibright-v2a";
-	public static final String MAIN_STYLE_FILE = MAIN_STYLE + ".zip";
+	public static final String MAIN_STYLE = "nutibright-v3";
+
 	protected TileDataSource vectorTileDataSource;
-    protected MBVectorTileDecoder vectorTileDecoder;
-    protected boolean persistentTileCache = false;
-    
-    // Style parameters
-    protected String vectorStyleName = MAIN_STYLE; // default style name, each style has corresponding .zip asset
-    protected String vectorStyleLang = "en"; // default map language
+	protected MBVectorTileDecoder vectorTileDecoder;
+	protected boolean persistentTileCache = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	// Style parameters
+	protected String vectorStyleName = MAIN_STYLE + ":default"; // default style name, each style has corresponding .zip asset
+	protected String vectorStyleLang = "en"; // default map language
 
-        // Update options
-        mapView.getOptions().setZoomRange(new MapRange(0, 20));
-        
-        // Set default base map - online vector with persistent caching
-        updateBaseLayer();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	Menu langMenu = menu.addSubMenu("Language");
-    	addLanguageMenuOption(langMenu, "English", "en");
-    	addLanguageMenuOption(langMenu, "German",  "de");
-        addLanguageMenuOption(langMenu, "Spanish",  "es");
-        addLanguageMenuOption(langMenu, "Italian",  "it");
-    	addLanguageMenuOption(langMenu, "French",  "fr");
-    	addLanguageMenuOption(langMenu, "Russian", "ru");
-    	addLanguageMenuOption(langMenu, "Chinese", "zh");
-   	
-    	Menu styleMenu = menu.addSubMenu("Style");
-    	addStyleMenuOption(styleMenu, "Basic", "basic");
-    	addStyleMenuOption(styleMenu, "NutiBright 2D", "nutibright-v2");
-    	addStyleMenuOption(styleMenu, "NutiBright 3D", "nutibright3d");
-    	addStyleMenuOption(styleMenu, "Loose Leaf", "looseleaf");
+		// Update options
+		mapView.getOptions().setZoomRange(new MapRange(0, 20));
 
-    	return true;
-    }
-    
-    private void addLanguageMenuOption(final Menu menu, String text, final String value) {
-    	MenuItem menuItem = menu.add(text).setOnMenuItemClickListener(new OnMenuItemClickListener(){
-    	    @Override
-    	    public boolean onMenuItemClick (MenuItem item){
-    	    	for (int i = 0; i < menu.size(); i++) {
-    	    		MenuItem otherItem = menu.getItem(i);
-    	    		if (otherItem == item) {
-    	    			otherItem.setIcon(android.R.drawable.checkbox_on_background);
-    	    		} else {
-    	    			otherItem.setIcon(null);
-    	    		}
-    	    	}
-    	    	vectorStyleLang = value;
-    	    	updateBaseLayer();
-    	        return true;
-    	    }
-    	});
-    	if (vectorStyleLang.equals(value)) {
-    		menuItem.setIcon(android.R.drawable.checkbox_on_background);
-    	}
-    }
-    
-    private void addStyleMenuOption(final Menu menu, String text, final String value) {
-    	MenuItem menuItem = menu.add(text).setOnMenuItemClickListener(new OnMenuItemClickListener(){
-    	    @Override
-    	    public boolean onMenuItemClick (MenuItem item){
-    	    	for (int i = 0; i < menu.size(); i++) {
-    	    		MenuItem otherItem = menu.getItem(i);
-    	    		if (otherItem == item) {
-    	    			otherItem.setIcon(android.R.drawable.checkbox_on_background);
-    	    		} else {
-    	    			otherItem.setIcon(null);
-    	    		}
-    	    	}
-    	    	vectorStyleName = value;
-    	    	updateBaseLayer();
-    	        return true;
-    	    }
-    	});
-    	if (vectorStyleName.equals(value)) {
-    		menuItem.setIcon(android.R.drawable.checkbox_on_background);
-    	}    	
-    }
-    
+		// Set default base map - online vector with persistent caching
+		updateBaseLayer();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		Menu langMenu = menu.addSubMenu("Language");
+
+		addLanguageMenuOption(langMenu, "English", "en");
+		addLanguageMenuOption(langMenu, "German",  "de");
+		addLanguageMenuOption(langMenu, "Spanish",  "es");
+		addLanguageMenuOption(langMenu, "Italian",  "it");
+		addLanguageMenuOption(langMenu, "French",  "fr");
+		addLanguageMenuOption(langMenu, "Russian", "ru");
+		addLanguageMenuOption(langMenu, "Chinese", "zh");
+
+		Menu styleMenu = menu.addSubMenu("Style");
+
+		addStyleMenuOption(styleMenu, "Basic", "basic");
+		addStyleMenuOption(styleMenu, "NutiBright 2D", MAIN_STYLE + ":default");
+		addStyleMenuOption(styleMenu, "Nutiteq Dark", MAIN_STYLE + ":nutiteq_dark");
+		addStyleMenuOption(styleMenu, "Nutiteq Grey", MAIN_STYLE + ":nutiteq_grey");
+		addStyleMenuOption(styleMenu, "NutiBright 3D", "nutibright3d");
+		addStyleMenuOption(styleMenu, "Loose Leaf", "looseleaf");
+
+		return true;
+	}
+
+	private void addLanguageMenuOption(final Menu menu, String text, final String value) {
+
+		MenuItem menuItem = menu.add(text).setOnMenuItemClickListener(new OnMenuItemClickListener(){
+			@Override
+			public boolean onMenuItemClick (MenuItem item){
+				for (int i = 0; i < menu.size(); i++) {
+					MenuItem otherItem = menu.getItem(i);
+					if (otherItem == item) {
+						otherItem.setIcon(android.R.drawable.checkbox_on_background);
+					} else {
+						otherItem.setIcon(null);
+					}
+				}
+				vectorStyleLang = value;
+				updateBaseLayer();
+				return true;
+			}
+		});
+
+		if (vectorStyleLang.equals(value)) {
+			menuItem.setIcon(android.R.drawable.checkbox_on_background);
+		}
+	}
+
+	private void addStyleMenuOption(final Menu menu, String text, final String value) {
+		MenuItem menuItem = menu.add(text).setOnMenuItemClickListener(new OnMenuItemClickListener(){
+			@Override
+			public boolean onMenuItemClick (MenuItem item){
+				for (int i = 0; i < menu.size(); i++) {
+					MenuItem otherItem = menu.getItem(i);
+					if (otherItem == item) {
+						otherItem.setIcon(android.R.drawable.checkbox_on_background);
+					} else {
+						otherItem.setIcon(null);
+					}
+				}
+				vectorStyleName = value;
+				updateBaseLayer();
+				return true;
+			}
+		});
+
+		if (vectorStyleName.equals(value)) {
+			menuItem.setIcon(android.R.drawable.checkbox_on_background);
+		}
+	}
+
     private void updateBaseLayer() {
+
+		/*
     	String styleAssetName = vectorStyleName + ".zip";
     	boolean styleBuildings3D = false;
     	if (vectorStyleName.equals("nutibright3d")) {
@@ -114,37 +125,63 @@ public class VectorMapSampleBaseActivity extends MapSampleBaseActivity {
     		styleBuildings3D = true;
     	}
         UnsignedCharVector styleBytes = AssetUtils.loadBytes(styleAssetName);
-        if (styleBytes != null){
-        	// Create style set
-            MBVectorTileStyleSet vectorTileStyleSet = new MBVectorTileStyleSet(styleBytes);
-            vectorTileDecoder = new MBVectorTileDecoder(vectorTileStyleSet);
-            
-            // Set language, language-specific texts from vector tiles will be used
-            vectorTileDecoder.setStyleParameter("lang", vectorStyleLang);
-            
-            // OSM Bright style set supports choosing between 2d/3d buildings. Set corresponding parameter.
-            if (styleAssetName.equals(MAIN_STYLE_FILE)) {
-            	vectorTileDecoder.setStyleParameter("buildings3d", styleBuildings3D);
-            	vectorTileDecoder.setStyleParameter("markers3d",styleBuildings3D ? "1" : "0");
-            	vectorTileDecoder.setStyleParameter("texts3d",styleBuildings3D ? "1" : "0");
-            }
-            
-            // Create tile data source for vector tiles
-            if (vectorTileDataSource == null) {
-            	vectorTileDataSource = createTileDataSource();
-            }
 
-            // Remove old base layer, create new base layer
-            if (baseLayer != null) {
-            	mapView.getLayers().remove(baseLayer);
-            }
-            baseLayer = new VectorTileLayer(vectorTileDataSource, vectorTileDecoder);
-            mapView.getLayers().insert(0, baseLayer);
-        } else {
-            Log.e(Const.LOG_TAG, "map style file must be in project assets: "+vectorStyleName);        	
-        }
+		// Create style set
+		MBVectorTileStyleSet vectorTileStyleSet = new MBVectorTileStyleSet(styleBytes);
+
+		vectorTileDecoder = new MBVectorTileDecoder(vectorTileStyleSet);
+		*/
+
+		boolean styleBuildings3D = false;
+
+		if (vectorStyleName.contains(":")) {
+
+			String[] split = vectorStyleName.split(":");
+			String fileName = split[0];
+			String styleName = split[1];
+
+			String styleAssetName = fileName + ".zip";
+			UnsignedCharVector styleBytes = AssetUtils.loadBytes(styleAssetName);
+			MBVectorTileStyleSet vectorTileStyleSet = new MBVectorTileStyleSet(styleBytes);
+
+			vectorTileDecoder = new MBVectorTileDecoder(vectorTileStyleSet, styleName);
+
+		} else {
+
+			if (vectorStyleName.equals("nutibright3d")) {
+				vectorStyleName = MAIN_STYLE;
+				styleBuildings3D = true;
+			}
+
+			String styleAssetName = vectorStyleName + ".zip";
+			UnsignedCharVector styleBytes = AssetUtils.loadBytes(styleAssetName);
+			MBVectorTileStyleSet vectorTileStyleSet = new MBVectorTileStyleSet(styleBytes);
+
+			vectorTileDecoder = new MBVectorTileDecoder(vectorTileStyleSet);
+		}
+
+		// Set language, language-specific texts from vector tiles will be used
+		vectorTileDecoder.setStyleParameter("lang", vectorStyleLang);
+
+		// OSM Bright style set supports choosing between 2d/3d buildings. Set corresponding parameter.
+		vectorTileDecoder.setStyleParameter("buildings3d", styleBuildings3D ? "1" : "0");
+		vectorTileDecoder.setStyleParameter("markers3d",styleBuildings3D ? "1" : "0");
+		vectorTileDecoder.setStyleParameter("texts3d",styleBuildings3D ? "1" : "0");
+
+		// Create tile data source for vector tiles
+		if (vectorTileDataSource == null) {
+			vectorTileDataSource = createTileDataSource();
+		}
+
+		// Remove old base layer, create new base layer
+		if (baseLayer != null) {
+			mapView.getLayers().remove(baseLayer);
+		}
+
+		baseLayer = new VectorTileLayer(vectorTileDataSource, vectorTileDecoder);
+		mapView.getLayers().insert(0, baseLayer);
     }
-    
+
     protected TileDataSource createTileDataSource() {
         TileDataSource vectorTileDataSource = new NutiteqOnlineTileDataSource("nutiteq.osm");
 
